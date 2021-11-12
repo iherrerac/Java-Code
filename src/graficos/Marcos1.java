0b306695128378d590dfebde848df912131b455d
iherrerac <isra.herrera@hotmail.com>
2021-11-12 18:02:10 +0100

    Frames, creamos uno para practicar y intentamos agregarle un boton

--- src/graficos/Marcos1.java
+++ src/graficos/Marcos1.java
@@ -1,8 +1,9 @@
 package graficos;
 
-import javax.swing.JFrame;//VEntanas
+import javax.swing.JFrame;//Ventanas
 import java.awt.Toolkit; // Sistema operativo
 import java.awt.Dimension; // Resolucion pantalla
+import java.awt.Image; // Clase para tratar imagenes
 
 public class Marcos1 {
 
@@ -13,18 +14,23 @@ public class Marcos1 {
 }
 
 class MarcoCentrado extends JFrame{
-	
+	//Constructor
 	public MarcoCentrado() {
 	setVisible(true);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setTitle("Mi marco centrado.");
 		
 	//Objeto Toolkit con datos del sistema
 	Toolkit MiPantalla = Toolkit.getDefaultToolkit();
 	//Metodo del objeto Toolkit para obtener la resolución
+	//Creamos una ventana centrada (posicion /4) y tamaño /2 de la resolucion.
 	Dimension MiResolucion = MiPantalla.getScreenSize();
 	int alturaPantalla = MiResolucion.height;
 	int anchoPantalla = MiResolucion.width;
-	setBounds(50,100,alturaPantalla/2,anchoPantalla/2);
+	setBounds(anchoPantalla/4,alturaPantalla/4,anchoPantalla/2,alturaPantalla/2);
+	Image MiIcono = MiPantalla.getImage("src/graficos/icono.jpg");
+	setIconImage(MiIcono);
+
 	
 	
 	}
