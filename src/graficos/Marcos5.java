0110adfa5d91b8c5703637f6e184dda7f250376d
iherrerac <isra.herrera@hotmail.com>
2021-11-23 12:55:31 +0100

    Fonts - Como Cambiar las fuentes con la Clase Fonts

--- src/graficos/Marcos5.java
+++ src/graficos/Marcos5.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Font;
 
 public class Marcos5 {
 
@@ -19,8 +20,13 @@ class M5 extends JFrame{
 	public M5() {
 		setVisible(true);
 		setTitle("Fuentes");
-		setBounds(200,200,600,400);
-		P5 MiPanel = new P5();		
+		setBounds(200,200,400,400);
+		P5 MiPanel = new P5();
+		//Establee color de fondo de la lamina
+		MiPanel.setBackground(Color.WHITE);
+		// Establece el de todo lo que se dibuje en la lamina
+		//MiPanel.setForeground(Color.RED);
+		add(MiPanel);
 	}
 }
 
@@ -28,7 +34,16 @@ class P5 extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
+		//Contructor de la clase Font (String name, int style (Constante tipo de letra), int size)
 		Font MiFuente = new Font("Arial", Font.BOLD, 26);
-		g2.drawString("Juan", 250,250);
+		g2.setFont(MiFuente);
+		g2.setColor(Color.BLUE);
+		g2.drawString("Juan",100,100);
+		
+		//Cambiar fuente esta vez declarada directamente en el metodo setFont
+		g2.setFont(new Font("Currier", Font.ITALIC, 26));
+		g2.setColor(Color.RED);
+		g2.drawString("Juan", 100,130);
+	
 	}
 }
