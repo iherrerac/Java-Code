a0bf2ffd46822f48e852722d3ef0d5eac3c5a8e1
iherrerac <isra.herrera@hotmail.com>
2021-12-10 00:26:21 +0100

    Menus: JMenubar, JMenu y JMenuItem.

---
+++ src/graficos/Menus.java
@@ -0,0 +1,60 @@
+package graficos;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import java.awt.BorderLayout;
+
+public class Menus {
+
+	public static void main(String[] args) {
+		Marco34 m34 = new Marco34();
+		m34.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
+class Marco34 extends JFrame{
+	public Marco34() {
+		setTitle("Menus");
+		setBounds(100,150,500,300);
+		add(new Panel34());
+		setVisible(true);
+	}
+}
+
+class Panel34 extends JPanel{
+	public Panel34() {
+		menu= new JMenuBar();
+		JMenu archivo = new JMenu("Archivo");
+		JMenu edicion = new JMenu("Edición");
+		JMenu herramientas = new JMenu("Herramientas");
+		JMenuItem guardar = new JMenuItem("Guardar");
+		JMenuItem guardarComo = new JMenuItem("Guardar como");
+		JMenuItem cortar = new JMenuItem("Cortar");
+		JMenuItem copiar = new JMenuItem("Copiar");
+		JMenuItem pegar = new JMenuItem("Pegar");
+		JMenu opciones = new JMenu("Opciones");
+		JMenuItem opcion1 = new JMenuItem("Opcion1");
+		JMenuItem opcion2 = new JMenuItem("Opcion2");
+		JMenuItem generales = new JMenuItem("Generales");
+		archivo.add(guardar);
+		archivo.add(guardarComo);
+		edicion.add(cortar);
+		edicion.add(copiar);
+		edicion.add(pegar);
+		edicion.addSeparator();//Separador
+		edicion.add(opciones);
+		opciones.add(opcion1);
+		opciones.add(opcion2);
+		herramientas.add(generales);
+		menu.add(archivo);
+		menu.add(edicion);
+		menu.add(herramientas);
+		add(menu);
+
+	}
+	private JMenuBar menu;
+}
