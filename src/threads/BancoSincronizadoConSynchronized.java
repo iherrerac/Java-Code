dd376329f8c827efcd423b480cf424cac514d8b4
iherrerac <isra.herrera@hotmail.com>
2022-01-11 11:37:25 +0100

    Threads Cierres Implicitos y Explicitos.

--- src/threads/BancoSincronizadoConSynchronized.java
+++ src/threads/BancoSincronizadoConSynchronized.java
@@ -12,20 +12,16 @@ import java.util.concurrent.locks.*;
  * el saldo total no se modificara .... o eso deberia pasar
  * ¿Por que se modifica el saldo total, cuando deberia ser siempre fija ( 200000 )
  * por que hay varias transferencias concurrentes.
- * Video 175 En este explican para la sincronizacion como usar la clase ReentrantLock que hereda 
- * de la Interface Lock, mismo uso que join() pero para casos mas complejos
- * Video 176 que pasa si la cantidad tranferida supera al saldo de la cuenta? Modificaremos 
- * el if else del metodo Tranferencia para explicar las condiciones
- * Video 177 Cierre Explicito, podemos establecer mas de una condicion. El Bloqueo Condicional con Condition y newCondition(),await(),signal();
- * 
+ * Video 178 Sincronizad agregamos la palabra sincronized en el metodo, wait()  para la condicion y 
+ * notifyAll() para despertar hilos. cierre Implicito SOLO UNA CONDICION
  */
 
-public class BancoSinSincronizar {
+public class BancoSincronizadoConSynchronized {
 
 	public static void main(String[] args) {
-		Banco b= new Banco();
+		Banco1 b1= new Banco1();
 		for ( int i=0; i<100;i++) {
-			EjecucionTransferencias r = new EjecucionTransferencias(b, i, 2000);
+			EjecucionTransferencias1 r = new EjecucionTransferencias1(b1, i, 2000);
 			Thread t = new Thread(r);
 			t.start();
 		}
@@ -33,26 +29,21 @@ public class BancoSinSincronizar {
 
 }
 
-class Banco{
-	public Banco() {
+class Banco1{
+	public Banco1() {
 		cuentas = new double[100];
 		for (int i=0; i< cuentas.length; i++) {
 			cuentas[i]= 2000;
 		}
-		//el bloqueo tiene qu eestablecese en base a una condicion. bloqueo.newCondition()
-		saldoSuficiente=cierreBanco.newCondition();
+
 	}
 	
-	public void transferencia(int cuentaOrigen,int cuentaDestino,double cantidad) throws InterruptedException {
-		cierreBanco.lock();//Bloqueamos hilo y rodeamos de try
-		try {
+	public synchronized void  transferencia(int cuentaOrigen,int cuentaDestino,double cantidad) throws InterruptedException {
+
 			//Controlar que la tranferencia no sea mayor que el saldo en cuenta
 			while (cantidad > cuentas[cuentaOrigen]) {
-				//if (cantidad > cuentas[cuentaOrigen]) {
-				//System.out.println("-----CANTIDAD INSUFICIENT: CUENTA "+cuentaOrigen+" ... SALDO: "+cuentas[cuentaOrigen] +"... CATIDAD: "+cantidad );
-				//return;
-				saldoSuficiente.await();//Pone el hilo a la espera si la transferencia supera al saldo
-			} //else System.out.println("----- CANTIDAD OK ------"+cuentaOrigen);
+				wait();//Si la condicion se cumple, hilo a la espera
+			} 
 			
 			//Imprimimos el hilo que hace la tranferencia como control
 			System.out.println(Thread.currentThread());
@@ -61,11 +52,8 @@ class Banco{
 			System.out.printf("%10.2f de %d para %d",cantidad,cuentaOrigen,cuentaDestino);
 			cuentas[cuentaDestino]+=cantidad; //Suma a la cuenta destino
 			System.out.printf(" Saldo total: %10.2f%n",getSaldoTotal());
-			saldoSuficiente.signal();//preguntamos si ya hay saldo para retomar el hilo en await()
-		}finally {//tanto si ocurre una excepcion o no haga esto ...
-			cierreBanco.unlock();
-		}
-	}
+			notifyAll(); //Revisar la condicion
+	}	
 	//metodo devuelve el saldo total
 	public double getSaldoTotal() {
 		double suma_cuentas = 0;
@@ -76,15 +64,12 @@ class Banco{
 	}
 	
 	private final double[] cuentas;
-	//Instanciamos cierreBanco para sincronizacion de hilos y poder llamar a metodos lock y unlock
-	private Lock cierreBanco= new ReentrantLock();
-	private Condition saldoSuficiente;
 }
 
 //Clase para los Threads de las transferencias
-class EjecucionTransferencias implements Runnable{
+class EjecucionTransferencias1 implements Runnable{
 	//variable de tipo banco, int cuentas origen, maximo de la transferencia
-	public EjecucionTransferencias (Banco b,int de,double max) {
+	public EjecucionTransferencias1 (Banco1 b,int de,double max) {
 		banco = b;
 		delaCuenta=de;
 		cantidadMax=max;
@@ -106,7 +91,7 @@ class EjecucionTransferencias implements Runnable{
 		
 	}
 	
-	private Banco banco;
+	private Banco1 banco;
 	private int delaCuenta;//cuenta Origen
 	private double cantidadMax;
 }
