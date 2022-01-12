7ebe51b4508aace25822cca62e6bc2c18e74ea4a
iherrerac <isra.herrera@hotmail.com>
2022-01-12 13:45:30 +0100

    Colleciones e iteradores

--- src/colecciones/Cliente.java
+++ src/colecciones/Cliente.java
@@ -1,5 +1,7 @@
 package colecciones;
 
+import java.util.Objects;
+
 /* 
  * Video 180 Uso de colecciones
  * Aplicacion Banco que usa una coleccion para almacenar los clientes
@@ -33,6 +35,25 @@ public class Cliente {
 	}
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
+	}
+
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(n_cuenta);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(n_cuenta, other.n_cuenta);
 	}
 
 
--- src/colecciones/CuentasUsuarios.java
+++ src/colecciones/CuentasUsuarios.java
@@ -10,6 +10,7 @@ import java.util.*;
  * En principio no necesitamos ordenacion para este ejemplo.
  * Video 181: Equals() y HasCode()
  * Video 182: Rescribiendo Equals() y HasCode()
+ * Video 183: Iteradores
  */
 
 public class CuentasUsuarios {
@@ -19,6 +20,7 @@ public class CuentasUsuarios {
 		Cliente cl2 = new Cliente("Rafael Nadal","00002",250000);
 		Cliente cl3 = new Cliente("Penelope Cruz","00003",300000);
 		Cliente cl4 = new Cliente("Julio Iglesias","00004",500000);
+		//No lo agrega, duplicado tras sobrescribir equals() y hashCode()
 		Cliente cl5 = new Cliente("Antonio Banderas","00001",200000);
 		
 		//Setes Interfaces, tenemos que implementar la clase que la implemente 
@@ -31,9 +33,32 @@ public class CuentasUsuarios {
 		clientesBanco.add(cl4);
 		clientesBanco.add(cl5);
 		
+		//Recorrer con bucle for each la coleccion, la imprime y elimina uno de ellos
 		for (Cliente cliente: clientesBanco) {
+
+			if (cliente.getNombre().equals("Julio Iglesias")) {
+				//clientesBanco.remove(cliente); //Lanza una excepcion, no se puede eliminar en un bucle
+			}
 			System.out.println(cliente.getNombre()+" "
 			+cliente.getN_cuenta()+" "+cliente.getSaldo());
+			
+		}
+		
+		//recorrer una coleccion con Iterator,  e imprime los nombre
+		Iterator<Cliente> it= clientesBanco.iterator();
+		while(it.hasNext()) {//Mientras halla elementos siguientes
+			//Hasta que no vayamos al siguiente elemento , no podemo saber el nombre del anterior
+			//Primero hay que saltar
+			String nombre_cliente = it.next().getNombre();
+			System.out.println(nombre_cliente);
+			
+		}
+		
+		Iterator<Cliente> it2= clientesBanco.iterator();//Creamos otro iterator el anterior ya no sirve
+		while(it2.hasNext()) {
+		// Creamos una copia del objeto y podemos tratar los diferentes campos con el
+		Cliente c = it2.next();
+		System.out.println(c.getNombre().concat(" "+c.getN_cuenta().concat(" "+c.getSaldo())));
 		}
 	}
 
