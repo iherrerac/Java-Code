d1f5daec45055e29fc82c3dc8d7c818f72581402
iherrerac <isra.herrera@hotmail.com>
2022-01-18 10:58:53 +0100

    Socket Chat

--- src/sockets/Cliente.java
+++ src/sockets/Cliente.java
@@ -4,18 +4,25 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import java.awt.event.*;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /*
- * Video 190,191: 
+ * Video 190,191,192,193,194,195; 
  * Sockets
  * Aplicacion chat cliente servidor . Para crear el socket necesitamos ip y el puerto
  * Video 191: Creacion de flujo de salida, 
+ * Video 192: Aplicacion con multiples clientes
+ * Video 193: Aplicacion con multiples clientes II. ObjectOutputStream
  */
 
 public class Cliente {
@@ -36,16 +43,26 @@ class MarcoCliente extends JFrame{
 	}
 }
 
-class LaminaMarcoCliente extends JPanel{
+class LaminaMarcoCliente extends JPanel implements Runnable{
 	public LaminaMarcoCliente() {
-		JLabel texto = new JLabel("CLIENTE");
+		nick = new JTextField(5);
+		add(nick);
+		JLabel texto = new JLabel("- CHAT -");
 		add(texto);
+		ip =  new JTextField(8);
+		add(ip);
+		campoChat = new JTextArea(12,20);
+		add(campoChat);
 		campo1 = new JTextField(20);
 		add(campo1);
 		miBoton= new JButton("Enviar");
 		EnviaTexto miEvento= new EnviaTexto();
 		miBoton.addActionListener(miEvento);
 		add(miBoton);
+		//Cuando la aplicacion se ejecuta es en la propia lamina en el constructor ponemos en funcionamiento el hilo
+		//Es la propia clase donde nos encontramos la que tiene este hilo/metodo run, por eso this
+		Thread miHilo = new Thread(this);
+		miHilo.start();
 	}
 	
 	private class EnviaTexto implements ActionListener{
@@ -53,15 +70,27 @@ class LaminaMarcoCliente extends JPanel{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			System.out.println(campo1.getText());// imprime el texto del JTextField
+			campoChat.append("\nYo: "+campo1.getText());
 			try {
 				//Comunicar con el servidor
 				Socket miSocket = new Socket("192.168.0.26",9999);
-				//Flujo de datos de salida que circula por el socket creado
-				DataOutputStream flujo_salida = new DataOutputStream(miSocket.getOutputStream());
-				//Escribe en el flujo lo que hay en el campo1, el flujo circulara por el socket 
-				//y se dirige al servidor indicado
-				flujo_salida.writeUTF(campo1.getText());
-				flujo_salida.close();//Cierramos flujo
+				//Video 192 enviamos nick, ip y mensaje.Empaquetamos datos
+				PaqueteEnvio datos = new PaqueteEnvio();
+				datos.setNick(nick.getText());
+				datos.setIp(ip.getText());
+				datos.setMensaje(campo1.getText());
+				
+				//Ya no enviamos texto si no un objeto:
+				ObjectOutputStream paquete_datos = new ObjectOutputStream(miSocket.getOutputStream());
+				paquete_datos.writeObject(datos);
+				miSocket.close();
+//				//Video 191 Solo envia el campo de JTextArea				
+//				//Flujo de datos de salida que circula por el socket creado
+//				DataOutputStream flujo_salida = new DataOutputStream(miSocket.getOutputStream());
+//				//Escribe en el flujo lo que hay en el campo1, el flujo circulara por el socket 
+//				//y se dirige al servidor indicado
+//				flujo_salida.writeUTF(campo1.getText());
+//				flujo_salida.close();//Cierramos flujo
 			} catch (IOException e1) {
 				System.out.println(e1.getMessage());// Cual ha sido el error?
 			}
@@ -69,6 +98,55 @@ class LaminaMarcoCliente extends JPanel{
 		
 	}
 	
-	private JTextField campo1;
+	@Override
+	public void run() {
+		try {
+			ServerSocket servidor_cliente = new ServerSocket(9090);
+			Socket cliente;
+			PaqueteEnvio paqueteRecibido;
+			
+			while(true) {
+			cliente= servidor_cliente.accept();
+			ObjectInputStream flujoEntrada = new ObjectInputStream(cliente.getInputStream());
+			paqueteRecibido = (PaqueteEnvio)flujoEntrada.readObject();
+			campoChat.append("\n"+paqueteRecibido.getNick()+": "+ paqueteRecibido.getMensaje());
+			}
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private JTextField campo1, nick, ip;
+	private JTextArea campoChat;
 	private JButton miBoton;
+}
+
+class PaqueteEnvio implements Serializable{ // Hay que serializar la clase para que pueda convertirse en datos
+	
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	private String nick, ip, mensaje;
 }
--- src/sockets/Servidor.java
+++ src/sockets/Servidor.java
@@ -6,14 +6,17 @@ import javax.swing.JTextArea;
 import java.awt.BorderLayout;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /*
  * Sockets 
- * Video 190,191
+ * Video 190,191,193,194,195
  * Video 191: Creacion de Thread con una tarea que realiza en segundo plano que permanezca a la
  * escucha del puerto 9999 permanentemente.
+ * Video 193: Aplicacion multicliente. Recibimos datos com oobeto
  */
 public class Servidor {
 
@@ -47,16 +50,32 @@ class MarcoServidor extends JFrame implements Runnable{
 		//Server socket argumento de tipo entero especificando que tiene que abrir
 		try {
 			ServerSocket servidor = new ServerSocket(9999);
+			//Video 193: para recibir objetos
+			String nick,ip,mensaje;
+			PaqueteEnvio paquete_recibido;
+			
 			//Permanezca a la escucha del puerto y acepte las peticiones
 			while(true) {//Bucle infinito en segundo plano para que siempre este a la escucha
 			Socket miSocket = servidor.accept();
-			//Creamos el flujo de entrada
-			DataInputStream flujo_entrada= new DataInputStream(miSocket.getInputStream());
-			String mensajeTexto= flujo_entrada.readUTF();
-			areaTexto.append("\n"+mensajeTexto);
+			ObjectInputStream paquete_datos = new ObjectInputStream(miSocket.getInputStream());
+			paquete_recibido = (PaqueteEnvio) paquete_datos.readObject();
+			nick = paquete_recibido.getNick();
+			ip = paquete_recibido.getIp();
+			mensaje = paquete_recibido.getMensaje();
+			areaTexto.append("\n" + nick + " : "+ mensaje + " para " + ip);
+			Socket enviaDestinatario = new Socket(ip,9090);
+			ObjectOutputStream paqueteReenvio = new ObjectOutputStream(enviaDestinatario.getOutputStream());
+			paqueteReenvio.writeObject(paqueteReenvio);
+			paqueteReenvio.close();
+			enviaDestinatario.close();
+//			//Video 191: Recibimos JTextArea del cliente
+//			//Creamos el flujo de entrada
+//			DataInputStream flujo_entrada= new DataInputStream(miSocket.getInputStream());
+//			String mensajeTexto= flujo_entrada.readUTF();
+//			areaTexto.append("\n"+mensajeTexto);
 			miSocket.close();
 			}
-		} catch (IOException e) {
+		} catch (IOException | ClassNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
 	}
